9e54057dc0f541d255529587a1f18df5d9b00a27
chrismenke45 <chrismenke45@gmail.com>
2023-05-17 16:51:35 -0700

    began laying out oroject file structure

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import indexRouter from './routes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server!');
-});
+
+app.use('/', indexRouter);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);

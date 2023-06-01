f5d9a181011f34bed0a400f9a43f112cb2813e06
chrismenke45 <chrismenke45@gmail.com>
2023-05-31 17:39:55 -0700

    added logger and json/form parser

--- app.ts
+++ app.ts
@@ -2,12 +2,16 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import bookRouter from './src/routes/book';
 import authRouter from './src/routes/auth';
+import logger from 'morgan';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', bookRouter);
 app.use('/auth', authRouter)

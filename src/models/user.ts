4d2160fbff3f10448ad70d22b51b826fe6293cfa
chrismenke45 <chrismenke45@gmail.com>
2023-06-16 11:34:58 -0700

    usernames must be unique

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,6 +4,8 @@ const userShema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique : true,
+        dropDups: true,
     },
     hashedPassword: {
         type: String,

4ddab2d2ddaa889c4c741e7c5092c4557734bd1e
chrismenke45 <chrismenke45@gmail.com>
2023-06-25 08:40:27 -0700

    has ISBN13 field and no _id field

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,17 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 const bookShema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique : true,
-        dropDups: true,
-    },
-    pageCount: {
-        type: Number,
-        required: true,
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  pageCount: {
+    type: Number,
+    required: true,
+  },
+  ISBN13: {
+    type: Number,
+    required: true,
+    unique: true,
+    dropDups: true,
+    min: 1000000000000,
+    max: 9999999999999,
+  },
+}, { _id: false })
 
-const BookModel = mongoose.model('Book', bookShema)
+const BookModel = mongoose.model("Book", bookShema)
 export { BookModel }
